d7689d322ebaaa1b205bf657c21e1185712e0981
Arthur Oliveira Xavier <33815004+aoxavier1@users.noreply.github.com>
2020-09-23 21:50:15 -0300

    Add files via upload

---
+++ Exercicio 07/Retangulo.java
@@ -0,0 +1,23 @@
+public class Retangulo{
+
+String largura;
+String comprimento;
+
+  public Retangulo(){}
+
+  public Retangulo(String largura){
+    this.largura = 0;
+
+  public Retangulo(String comprimento){
+    this.comprimento = 0;
+  }
+  public Retangulo(String largura, String comprimento){
+    this.largura = largura;
+    this.comprimento = comprimeto;
+  }
+  public String getLargura() {
+      return largura;
+  
+  }
+  
+}
